f192c9bfa708aec79f29d29d894cfb4a85c5dc06
AlvarDev <deivid07.21@gmail.com>
2015-04-11 05:54:34 -0500

    register

--- Android/ShopMedical/app/src/main/java/com/alvardev/demos/shopmedical/LoginActivity.java
+++ Android/ShopMedical/app/src/main/java/com/alvardev/demos/shopmedical/LoginActivity.java
@@ -1,17 +1,39 @@
 package com.alvardev.demos.shopmedical;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.alvardev.demos.shopmedical.view.BaseActionBarActivity;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 
 public class LoginActivity extends BaseActionBarActivity {
+
+    @InjectView(R.id.btnRegister) Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+        ButterKnife.inject(this);
+        setComponents();
+    }
+
+    private void setComponents(){
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 
--- Android/ShopMedical/app/src/main/java/com/alvardev/demos/shopmedical/RegisterActivity.java
+++ Android/ShopMedical/app/src/main/java/com/alvardev/demos/shopmedical/RegisterActivity.java
@@ -1,39 +1,55 @@
 package com.alvardev.demos.shopmedical;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.alvardev.demos.shopmedical.view.BaseActionBarActivity;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
-public class RegisterActivity extends ActionBarActivity {
+public class RegisterActivity extends BaseActionBarActivity {
+
+    @InjectView(R.id.spiDistritos) Spinner spiDistritos;
+    @InjectView(R.id.btnSaveRegister) Button btnSaveRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+        ButterKnife.inject(this);
+        setComponents();
     }
 
+    private void setComponents(){
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_register, menu);
-        return true;
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.distritos, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spiDistritos.setAdapter(adapter);
+
+        btnSaveRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),DashboardActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
